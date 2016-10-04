7376f9b95a4b84169f15586e28a694fee286dcc2
TJ Bindseil <bindseil@wisc.edu>
2016-10-03 21:02:17 -0500

    wrote code to drive, then stop after waiting with wait time command, tomorrow looking to implement script class

--- csharp/TurtlebotLibrary/TurtlebotLibrarySharpTester/Program.cs
+++ csharp/TurtlebotLibrary/TurtlebotLibrarySharpTester/Program.cs
@@ -12,13 +12,51 @@ namespace TurtlebotLibrarySharpTester
     {
         static void Main(string[] args)
         {
-            Drive d = new Drive(100, 50);
-            byte[] buffer = d.Serialize();
 
-            foreach (byte b in buffer)
-                Console.Write("{0:X} ", b);
-            Console.WriteLine();
+            //            Drive d = new Drive(100, 50);
+            //            byte[] buffer = d.Serialize();
+            //
+            //            foreach (byte b in buffer)
+            //                Console.Write("{0:X} ", b);
+            //            Console.WriteLine();
+            //
+            //            Console.Read();
 
+            string serialPort = "";
+            bool testMode = false;
+
+            for (int index = 0; index < args.Length; ++index)
+            {
+                if (args[index][0] != '-')
+                {
+                    continue;
+                }
+
+                switch (args[index][1])
+                {
+                    case 's':
+                        ++index;
+                        if (index == args.Length)
+                            return;
+                        serialPort = args[index];
+                        break;
+                    case 't':
+                        testMode = true;
+                        break;
+                    default:
+                        return;
+                }
+            }
+
+            TurtlebotCommandSender c = new TurtlebotCommandSender();
+            c.Initialize(serialPort, DriveMode.Safe);
+            Drive driveMessage = new Drive((int)100, (int)Drive.STRAIGHT);
+            c.SendTurtlebotMessage(driveMessage);
+            WaitTime w = new WaitTime(10);
+            c.SendTurtlebotMessage(w);
+            System.Threading.Thread.Sleep(5000);
+            Drive stop = new Drive((int)0, (int)Drive.STRAIGHT);
+            c.SendTurtlebotMessage(stop);
             Console.Read();
         }
     }
